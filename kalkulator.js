14dad0398f12df9e402de0fb757d9fbe3e1ed238
sordadros <88931738+sordadros@users.noreply.github.com>
2021-08-22 14:02:55 +0700

    harusnya uda bisa

--- kalkulator.js
+++ kalkulator.js
@@ -17,34 +17,79 @@ function clearCalculator(){
 }
 
 function inputAngka(angka){ /*user input angka*/
-    if(calculator.displayNumber!=0){
-        calculator.displayNumber += angka;
-    }else{
+    if(calculator.displayNumber === '0'){
         calculator.displayNumber = angka;
-    }
-     /*ngejelasin fungsi ini
+     } else {
+         calculator.displayNumber += angka;
+     } /*ngejelasin fungsi ini
     yaitu ngubah tampilan display pas dapet input*/
-    
 }
 
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+        return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+ }
 
-window.addEventListener("load", function () {
-    
-    const buttons = document.querySelectorAll/*returns semua matches*/(".button");
-    for (let button of buttons)/*looping nilai utk kasi even klik di tiap buton*/ {
+ function handleOperator(operator) {
+    if (!calculator.waitingForSecondNumber) {
+        calculator.operator = operator;
+        calculator.waitingForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+ 
+        // mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+        calculator.displayNumber = '0';
+    } else {
+        alert('Anda sudah menetapkan operator.')
+    }
+}
+
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+        alert("Anda belum menetapkan operator");
+        return;
+    }
+  
+    let result = 0;
+    if (calculator.operator === "+") {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
+    }
+  
+    calculator.displayNumber = result;
+ }
+ 
+window.addEventListener("load", function(){
+const buttons = document.querySelectorAll/*returns semua matches*/(".button");
+for (let button of buttons)/*looping nilai utk kasi even klik di tiap buton*/ {
     button.addEventListener('click', function(event){
         //dapetin objek elemen yg diklik
         
+        const bb = event.target;
 
-        const b = event.target;
-        
-        var in_user = b.innerHTML
-        if(in_user=="CE"){
-            calculator.displayNumber = 0;
-        }else{
-            inputAngka(in_user);
+        if (bb.classList.contains('clear')) {
+            clearCalculator();
+            updateDisplay();
+            return;
+        } 
+        if(bb.classList.contains('negative')) {
+            inverseNumber();
+            updateDisplay();
+            return;
         }
-        updateDisplay();
+        if(bb.classList.contains('equals')) {
+            performCalculation();
+            updateDisplay();
+            return;
+        }
+        if(bb.classList.contains('operator')) {
+            handleOperator(bb.innerHTML);
+            return;
+        }
+        inputAngka(bb.innerHTML);
+        updateDisplay()
     });
 }
-});
+})
